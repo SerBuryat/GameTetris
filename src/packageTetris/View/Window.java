94268bcb5480f92c4a135b2473a18b7f80b73ad9
SerBuryat <tema.anosov@yandex.ru>
2019-08-09 16:47:28 +0400

    Starting to make optimisation in Window.class

--- src/packageTetris/View/Window.java
+++ src/packageTetris/View/Window.java
@@ -3,10 +3,10 @@ package packageTetris.View;
 import javax.swing.*;
 import java.awt.*;
 
-public class Window {
-    public static final int WIDTH = 545, HEIGHT = 960; // optimal is W = 545; H = 960
-    GameField gameField;
-    Score score;
+public class Window extends JFrame {
+    private static final int WIDTH = 545, HEIGHT = 960; // optimal is W = 545; H = 960
+    private GameField gameField;
+    private Score score;
 
     public Window(GameField gameField, Score score) {
         this.gameField = gameField;
@@ -14,14 +14,18 @@ public class Window {
     }
 
     public void initialize() {
-        JFrame window = new JFrame("Tetris by SerButryat");
-        window.setSize(WIDTH, HEIGHT);
-        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        window.setResizable(false);
-        window.setLocationRelativeTo(null);
-        window.setVisible(true);
-        window.setLayout(new BorderLayout());
-        window.add(gameField, BorderLayout.CENTER);
-        window.add(score,BorderLayout.NORTH);
+        setTitle("Tetris by SerButryat");
+        setSize(WIDTH, HEIGHT);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setResizable(false);
+        setLocationRelativeTo(null);
+        setVisible(true);
+        setLayout(new BorderLayout());
+        setGameField();
+    }
+
+    public void setGameField() {
+        add(gameField, BorderLayout.CENTER);
+        add(score,BorderLayout.NORTH);
     }
 }
